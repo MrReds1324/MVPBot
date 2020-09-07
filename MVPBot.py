fb9ed05accabd3f92fcead62fd7c4ad31f204ee5
MrReds1324 <ryanb27@gmail.com>
2020-09-07 03:31:27 -0400

    Initial commit

---
+++ MVPBot.py
@@ -0,0 +1,70 @@
+import logging
+import os
+from datetime import datetime, timedelta
+
+from discord import Embed, HTTPException
+from discord.ext import commands
+from dotenv import load_dotenv
+
+from google_sheets import get_sheet_data
+
+load_dotenv()
+logger = logging.getLogger('discord')
+logger.setLevel(os.getenv('LOGGING_LEVEL'))
+handler = logging.FileHandler(filename='err.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+token = os.getenv('DISCORD_TOKEN')
+spreadsheet_id = os.getenv('SPREADSHEET_ID')
+
+bot = commands.Bot(command_prefix='!!')
+
+def get_tables():
+    current_date = datetime.utcnow()
+    trigger_date = datetime.utcnow().replace(hour=21, minute=0, second=0)
+    tomorrow = None
+    if current_date >= trigger_date:
+        tomorrow = current_date + timedelta(days=1)
+
+def build_embed_from_sheet(sheet):
+    sheet_embed = Embed(title=f'Upcoming MVPS - {datetime.utcnow().strftime("%D %I:%H %p")} UTC')
+    for line in sheet:
+        sheet_embed.add_field(name=f'{line[5]} UTC - {line[6]} PST - {line[7]} CEST - {line[9]} AEST', value=f'Location: {line[3]}', inline=False)
+    return sheet_embed
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.event
+async def on_message(message):
+    # Dont track the bots messages or let the bot issue commands
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('!!'):
+        await bot.process_commands(message)
+        return
+
+
+@bot.command(name='mvp', help='Show the current MVP list')
+async def update_prefix(ctx):
+    table = get_sheet_data('9/4/20!A:M')
+    await ctx.send(embed=build_embed_from_sheet(table))
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+    elif isinstance(error, HTTPException):
+        ctx.send('Something went wrong!')
+    else:
+        await ctx.send('An error occurred! Please try again')
+        print(error)
+        logger.error('{}: MESSAGE: {}'.format(error, ctx.message.content))
+
+
+bot.run(token)
---
+++ google_sheets.py
@@ -0,0 +1,103 @@
+import os.path
+import pickle
+from datetime import datetime
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+
+def filter_sheet(filter_date, mvp_sheet):
+    filtered_sheet = []
+    for mvp_row in mvp_sheet[2:]:
+        new_time = datetime.strptime(mvp_row[5], "%H:%M %p").time()
+        new_datetime = datetime.combine(filter_date.date(), new_time)
+        if new_datetime >= filter_date and mvp_row[3]:
+            filtered_sheet.append(mvp_row)
+    return filtered_sheet
+
+def get_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('sheets', 'v4', credentials=creds)
+
+
+def create_sheet(sheet_name, spreadsheet_id):
+    try:
+        sheet = get_service().spreadsheets()
+        batch_update_spreadsheet_request_body = {"requests": [{"addSheet": {"properties": {"title": sheet_name}}}]}
+        request = sheet.batchUpdate(spreadsheetId=spreadsheet_id, body=batch_update_spreadsheet_request_body).execute()
+        return True
+    except:
+        return False
+
+
+def get_sheetid(sheet_name, spreadsheet_id):
+    try:
+        sheet = get_service().spreadsheets()
+        result = sheet.get(spreadsheetId=spreadsheet_id).execute()
+        for sheet in result.get('sheets', []):
+            sheet_properties = sheet.get('properties', {})
+            if sheet_properties.get('title', '') == sheet_name:
+                return sheet_properties.get('sheetId', None)
+    except:
+        return None
+
+
+def copy_paste(source_id, destination_id, spreadsheet_id):
+    try:
+        sheet = get_service().spreadsheets()
+        batch_update_spreadsheet_request_body = {"requests": [{"copyPaste": {
+            "source": {
+                "sheetId": source_id,
+                "startRowIndex": 0,
+                "endRowIndex": 120,
+                "startColumnIndex": 0,
+                "endColumnIndex": 25
+            },
+            "destination": {
+                "sheetId": destination_id,
+                "startRowIndex": 0,
+                "endRowIndex": 120,
+                "startColumnIndex": 0,
+                "endColumnIndex": 25
+            },
+            "pasteType": "PASTE_NORMAL",
+            "pasteOrientation": "NORMAL"}}]}
+        request = sheet.batchUpdate(spreadsheetId=spreadsheet_id, body=batch_update_spreadsheet_request_body)
+        response = request.execute()
+        return True
+    except:
+        return False
+
+
+def get_sheet_data(get_range, spreadsheet_id):
+    try:
+        sheet = get_service().spreadsheets()
+        result = sheet.values().get(spreadsheetId=spreadsheet_id, range=get_range).execute()
+        return result.get('values', [])
+    except:
+        return []
+
+
+get_sheetid('')
+# get_sheet_data()
